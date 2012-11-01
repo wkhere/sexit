1e218f15db7d6dd11ebda9f6a15a5b24fc904fec
Wojciech Kaczmarek <wojtekk@kofeina.net>
2012-11-01 15:45:08 +0100

    FIX 27bed0e - looks like sex binary cant be run using relative path, using absolute one

--- sex.py
+++ sex.py
@@ -2,10 +2,13 @@
 # (c) 2012 wisdio SA
 # by Wojciech Kaczmarek frk@wisdio.com
 
+from os import environ
 from popen2 import popen2
 from transform import Transformer
 
-SexPath = './sex'
+PluginPath = environ['HOME'] + '/Library/Application\ Support/Sublime\ Text\ 2/Packages/User'
+# ^^ spaces must be bashslashed for popen2
+SexPath = PluginPath + '/sex'
 
 def do_sex(s0):
   i,o = popen2(SexPath)
